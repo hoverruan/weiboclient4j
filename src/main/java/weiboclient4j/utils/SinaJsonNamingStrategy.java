3fa4eb483185728c444ccef601cd3002acb45421
Hover Ruan <hoverruan@gmail.com>
2012-06-29 11:00:28 +0800

    SinaJsonNamingStrategy extends PropertyNamingStrategyBase to reduce codes

--- src/main/java/weiboclient4j/utils/SinaJsonNamingStrategy.java
+++ src/main/java/weiboclient4j/utils/SinaJsonNamingStrategy.java
@@ -1,27 +1,13 @@
 package weiboclient4j.utils;
 
-import org.codehaus.jackson.map.MapperConfig;
 import org.codehaus.jackson.map.PropertyNamingStrategy;
-import org.codehaus.jackson.map.introspect.AnnotatedField;
-import org.codehaus.jackson.map.introspect.AnnotatedMethod;
 
 /**
-* @author Hover Ruan
-*/
-public class SinaJsonNamingStrategy extends PropertyNamingStrategy {
-    @Override
-    public String nameForField(MapperConfig<?> config, AnnotatedField field, String defaultName) {
-        return convertName(defaultName);
-    }
-
-    @Override
-    public String nameForGetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
-        return convertName(defaultName);
-    }
-
-    @Override
-    public String nameForSetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
-        return convertName(defaultName);
+ * @author Hover Ruan
+ */
+public class SinaJsonNamingStrategy extends PropertyNamingStrategy.PropertyNamingStrategyBase {
+    public String translate(String propertyName) {
+        return convertName(propertyName);
     }
 
     String convertName(String defaultName) {
