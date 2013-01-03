3071a6fa6845b734e554821632fc3a435d852a61
Hover Ruan <hoverruan@gmail.com>
2013-01-03 08:44:02 +0800

    Add ParseWeiboErrorTest

---
+++ src/test/java/weiboclient4j/model/ParseWeiboErrorTest.java
@@ -0,0 +1,22 @@
+package weiboclient4j.model;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import weiboclient4j.WeiboError;
+import weiboclient4j.utils.JsonUtils;
+
+/**
+ * @author Hover Ruan
+ */
+public class ParseWeiboErrorTest extends AbstractParseJsonTest {
+    @Test
+    public void testParseWeiboError() throws Exception {
+        String content = readResource("/weiboclient4j/model/error.json");
+
+        WeiboError error = JsonUtils.readValue(content, WeiboError.class);
+        assertThat(error, is(notNullValue()));
+        assertThat(error.getErrorCode(), is("403"));
+    }
+}
